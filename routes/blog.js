88cd5b453ea9bfc1000622ae753c3536fb79c8bb
lcranf@gmail.com <lcranf@gmail.com>
2012-12-30 20:26:21 -0500

    got addBlog working and talking to MongoLabs

--- routes/blog.js
+++ routes/blog.js
@@ -23,9 +23,19 @@ Blog.prototype = {
 	addBlog: function(req, res) {
 		var name = req.body.name;
 
+		var newBlog = new blogModel();
+
+		newBlog.name = name;
+
+		newBlog.save(function(err) {
+			if(err) 
+				throw err;
+			res.json({ isvalid: true, message: "blog is saved with id of " + newBlog._id });
+		});
+
 		console.log("Name is " + name);
 
-		res.json({ isvalid: true, message: "blog is saved for " + name });
+		//res.json({ isvalid: true, message: "blog is saved for " + name });
 	},
 
 	deleteBlog: function(req, res) {
